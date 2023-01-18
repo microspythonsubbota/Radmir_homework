0acc2c31575540267dcec61268d1951c64be4d73
Radmir <radmirmusaev0766@gmail.com>
2023-01-18 18:58:35 +0500

    Задание №2 Подзадание a)
    Добавление конструкции try exept

--- Lesson_13 _homework/lesson_13_Exercise_2.py
+++ Lesson_13 _homework/lesson_13_Exercise_2.py
@@ -13,7 +13,12 @@ with open('from_csv_json_history_data.json', mode='w', encoding='UTF-8') as file
 for line in json_history_data:
     speed = line['Traffic In (speed)']
     speed2 = speed.split(" ")
-    gig = speed2[0]
-    mbit = speed2[1]
-    gbitsmall = mbit[:1]
-    print(f"Скорость соединения {gig}.{gbitsmall} Gbit/s")
+    try:
+        gig = speed2[0]
+        mbit = speed2[1]
+        gbitsmall = mbit[:1]
+        print(f"Скорость соединения {gig}.{gbitsmall} Gbit/s")
+    except IndexError:
+        continue
+
+#IndexError: list index out of range
