2279553934461dc5657d1eb85862fc911b3915b0
Radmir <radmirmusaev0766@gmail.com>
2023-01-18 18:54:08 +0500

    Задание №2 Подзадание a)

---
+++ Lesson_13 _homework/lesson_13_Exercise_2.py
@@ -0,0 +1,19 @@
+import csv
+import json
+from pprint import pprint
+
+json_history_data = []
+
+with open('historicdata.csv') as file:
+    reader = csv.DictReader(file)
+    for line in reader:
+        json_history_data.append(line)
+with open('from_csv_json_history_data.json', mode='w', encoding='UTF-8') as file:
+    json.dump(json_history_data, file, ensure_ascii=False, indent=4)
+for line in json_history_data:
+    speed = line['Traffic In (speed)']
+    speed2 = speed.split(" ")
+    gig = speed2[0]
+    mbit = speed2[1]
+    gbitsmall = mbit[:1]
+    print(f"Скорость соединения {gig}.{gbitsmall} Gbit/s")
